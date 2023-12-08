3d0cdca30feb5d36e636a2033a9deb91a6ed6742
Diehan David Drotschie <25896091@sun.ac.za>
2023-12-08 19:29:25 +0200

    javascript added

---
+++ script.js
@@ -0,0 +1,12 @@
+function flipCard() {
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+
+    if(name && surname) {
+        document.getElementById("tableNumber").innerText = `Hello, ${name} ${surname}`;
+        var card = document.querySelector('.flip-card-inner');
+        card.style.transform = 'rotateY(180deg)';
+    } else {
+        alert("Please enter both name and surname");
+    }
+}
