f7c3d582ffeb3dd9f4ad2ab451146621fd989194
Diehan David Drotschie <25896091@sun.ac.za>
2023-12-10 15:42:37 +0200

    working website

--- script.js
+++ script.js
@@ -4,26 +4,29 @@ function flipCard() {
 
     if (name && surname) {
        /* document.getElementById("tableNumber").innerText = `Welcome, ${name} ${surname} and (Date)`;*/
-        var card = document.querySelector('.flip-card-inner');
-        card.style.transform = 'rotateY(180deg)';
 
 
-        fetch('/searchGuest', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-        },
-            body: JSON.stringify({ name: name, surname: surname }),
-        })
-            .then(res => res.json())
-            .then(data => {
-                  if (data.length > 0) {
-                // Assuming data returns an array and the table number is in the first result
-                    var tableNumber = data[0].TableNumber; // Replace 'TableNumber' with the actual field name from your database
-                    document.getElementById("tableNumber").innerText = `Table Number: ${Table_Number}`;
-                } else {
-                    document.getElementById("tableNumber").innerText = "Guest Not Found";
-                }
+        /*TODO: Change so that upper and lowercase will work
+                Change so that works on mobile
+        */
+        fetch('https://raw.githubusercontent.com/DiehanDrotschie/Troue/main/Guests.json')
+        .then(response => response.json())
+         .then(data => {
+            const user = data.find(user => user.Name === name && user.Surname === surname);
+            const user2 = data.find(user => user.Date_Name === name && user.Date_Surname === surname);
+             if (user) {
+                var card = document.querySelector('.flip-card-inner');
+                card.style.transform = 'rotateY(180deg)';
+                document.getElementById("tableNumber").innerText = `Welcome: ${user.Name} and ${user.Date_Name}`;
+                document.getElementById("Number").innerText = `${user.Table_Number} `;
+            } else if (user2) {    
+                var card = document.querySelector('.flip-card-inner');
+                card.style.transform = 'rotateY(180deg)';
+                document.getElementById("tableNumber").innerText = `Welcome: ${user2.Name} and ${user2.Date_Name}`;
+                document.getElementById("Number").innerText = `${user2.Table_Number} `;
+            } else {
+                alert("Incorrect Name or Surname");
+            }
         });
 
     } else {
