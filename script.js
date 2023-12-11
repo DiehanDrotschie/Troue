129848c8b81e57730429e09ed107cb9666e07d62
Diehan David Drotschie <25896091@sun.ac.za>
2023-12-11 13:51:21 +0200

    small changes

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ function flipCard() {
     var surname = document.getElementById("surname").value;
 
     if (name && surname) {
-       /* document.getElementById("tableNumber").innerText = `Welcome, ${name} ${surname} and (Date)`;*/
-
-
-        /*TODO: Change so that upper and lowercase will work
-                Change so that works on mobile
-        */
         fetch('https://raw.githubusercontent.com/DiehanDrotschie/Troue/main/Guests.json')
         .then(response => response.json())
             .then(data => {
@@ -20,9 +14,9 @@ function flipCard() {
                 var card = document.querySelector('.flip-card-inner');
                 card.style.transform = 'rotateY(180deg)';
                  if (user.Date_Name == null) {
-                    document.getElementById("tableNumber").innerText = `Welcome: ${user.Name} and ${user.Surname}`;
+                    document.getElementById("userName").innerText = `${user.Name} and ${user.Surname}`;
                 } else {
-                    document.getElementById("tableNumber").innerText = `Welcome: ${user.Name} and ${user.Date_Name}`;               
+                    document.getElementById("userName").innerText = `${user.Name} and ${user.Date_Name}`;               
                 }
 
                 document.getElementById("Number").innerText = `${user.Table_Number} `;
@@ -31,9 +25,9 @@ function flipCard() {
                  card.style.transform = 'rotateY(180deg)';
                  /*Check if person has a date*/
                    if (user2.Date_Name == null) {
-                    document.getElementById("tableNumber").innerText = `Welcome: ${user2.Name} and ${user2.Surname}`;
+                    document.getElementById("userName").innerText = `${user2.Name} and ${user2.Surname}`;
                 } else {
-                    document.getElementById("tableNumber").innerText = `Welcome: ${user2.Name} and ${user2.Date_Name}`;               
+                    document.getElementById("userName").innerText = `${user2.Name} and ${user2.Date_Name}`;               
                 }
                 document.getElementById("Number").innerText = `${user2.Table_Number} `;
             } else {
